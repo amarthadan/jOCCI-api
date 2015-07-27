293b0f213c900e4e2018f6ad73ec0affdf55fe4a
Michal Kimle <kimle.michal@gmail.com>
2015-07-27 22:21:00 +0200

    fixed previuosly changed code for KeystoneAuthentication

--- src/main/java/cz/cesnet/cloud/occi/api/http/auth/KeystoneAuthentication.java
+++ src/main/java/cz/cesnet/cloud/occi/api/http/auth/KeystoneAuthentication.java
@@ -89,9 +89,15 @@ public class KeystoneAuthentication extends HTTPAuthentication {
 
         URI keystoneURI = URI.create(matcher.group(GROUP_URI));
         HttpHost target = new HttpHost(keystoneURI.getHost(), keystoneURI.getPort(), keystoneURI.getScheme());
+        //TODO
+        //this path normalization should be handled in a better way
         String path = keystoneURI.getPath();
         if (path == null) {
             path = "";
+        }
+
+        if (path.endsWith("/")) {
+            path = path.substring(0, path.length() - 1);
         }
 
         if (!path.endsWith(PATH_DEFAULT)) {
